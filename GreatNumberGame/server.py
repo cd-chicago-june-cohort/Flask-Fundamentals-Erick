387de3c30149442b2c57ab241279adc75fb99b81
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-07 18:40:39 -0500

    Number Game

---
+++ GreatNumberGame/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+
+app = Flask(__name__)
+app.secret_key = ('ThisIsARandomKey')
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def guess():
+
+    session['randomNum'] = random.randrange(1, 101)
+    session['guess'] = int(request.form['guess'])
+
+    print session['guess']
+    print session['randomNum']
+
+    if(session['guess'] == session['randomNum']):
+        return render_template('guess.html', guessNum = str(session['randomNum']) + ' was the number!')
+    elif(session['guess'] < session['randomNum']):
+        return render_template('guess.html', guessNum = 'Too low!')
+    elif(session['guess'] > session['randomNum']):
+        return render_template('guess.html', guessNum = 'Too high!')
+        
+app.run(debug = True)
+
---
+++ GreatNumberGame/static/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#submit').click(function(){
+        var guess = document.getElementById('#submit').value;
+        console.log(guess);
+    });
+    
+});
