fe613c5b31b24ce03d90f0942dff4cfbb6c995c0
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-07-07 19:11:00 -0500

    another bug fix

--- GreatNumberGame/server.py
+++ GreatNumberGame/server.py
@@ -7,23 +7,22 @@ app.secret_key = ('ThisIsARandomKey')
 
 @app.route('/')
 def main():
+    session['randomNum'] = random.randrange(1, 101)
     return render_template('index.html')
 
 @app.route('/guess', methods=['POST'])
 def guess():
-
-    session['randomNum'] = random.randrange(1, 101)
     session['guess'] = int(request.form['guess'])
-
     print session['guess']
     print session['randomNum']
-
     if(session['guess'] == session['randomNum']):
-        return render_template('guess.html', guessNum = str(session['randomNum']) + ' was the number!')
+        return render_template('newGame.html', guessNum = str(session['randomNum']) + ' was the number!')
     elif(session['guess'] < session['randomNum']):
         return render_template('guess.html', guessNum = 'Too low!')
     elif(session['guess'] > session['randomNum']):
         return render_template('guess.html', guessNum = 'Too high!')
-        
+@app.route('/winner')
+def winner():
+    return redirect('/')
 app.run(debug = True)
 
--- GreatNumberGame/static/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#submit').click(function(){
-        var guess = document.getElementById('#submit').value;
-        console.log(guess);
-    });
-    
-});
